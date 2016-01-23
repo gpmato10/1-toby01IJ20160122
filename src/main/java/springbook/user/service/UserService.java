bbeb223cf6e51b65f4d4336515eb9707af17ef79
dw <gpmato10@gmail.com>
2016-01-23 13:30:01 +0900

    5.1.3 p334 리스트 5-20 사용자 레벨 업그레이드 테스트 END.

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,6 +1,10 @@
 package springbook.user.service;
 
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.List;
 
 /**
  * Created by dw on 2016. 1. 23..
@@ -10,5 +14,28 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            Boolean changed = null;
+
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) { // BASIC -> SILVER 업그레이드
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true; // SILVER -> GOLD 업그레이드
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;    // 레벨 변경이 없음.
+            }
+            else  {
+                changed = false;
+            }
+            if (changed) {
+                userDao.update(user);   // 레벨변경이 있는 경우에만 update() 호출
+            }
+        }
     }
 }
